6b6345884d9709cb9d1f89133494248d34ebad00
Chen Haoquan <chenhq79@mail2.sysu.edu.cn>
2025-05-25 22:06:15 +0800

    更新环境配置脚本

---
+++ build-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt install -y python3 python3-pip python3-venv redis podman buildah skopeo
+
+python3 -m venv venv
+. venv/bin/activate
+pip install 'flask[async]' gunicorn redis cryptography httpx locust \
+    'flask-openapi3[swagger, redoc, rapidoc, rapipdf, scalar, elements]' \
+    coverage
