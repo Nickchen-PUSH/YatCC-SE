2453cb66549ee4e9e115c010061c2876cd9d724d
cho1x <wuxianbin0@gmail.com>
2025-07-11 13:27:24 +0800

    去掉不需要的容量表,添加logo

--- stu-site/src/components/StatusPanel/DashBoard.vue
+++ stu-site/src/components/StatusPanel/DashBoard.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import type { CodeSpaceInfo } from '@/types'
-import { formatBytes, formatTime } from '@/composables/utils/format'
+import { formatTime } from '@/composables/utils/format'
 
 const props = defineProps<{
   codespaceInfo: CodeSpaceInfo
 }>()
 
-const space_percentage = computed(() => {
-  return props.codespaceInfo.space_quota === 0
-    ? 100
-    : (props.codespaceInfo.space_used / props.codespaceInfo.space_quota) * 100
-})
+
 const time_percentage = computed(() => {
   return props.codespaceInfo.time_quota === 0
     ? 0
@@ -30,22 +26,12 @@ const colors = [
 
 <template>
   <div class="flex justify-center items-center gap-4 flex-col">
-    <el-progress type="dashboard" :percentage="space_percentage"
-    class="flex justify-center items-center" :color="colors">
-      <template #default>
-        <div class="text-xl">存储空间</div>
-        <div class="text-12px">
-          {{ formatBytes(codespaceInfo.space_used) }} /
-          {{ formatBytes(codespaceInfo.space_quota) }}
-        </div>
 
-      </template>
-    </el-progress>
     <el-progress type="dashboard" :percentage="time_percentage"
-    class="flex justify-center items-center" :color="colors">
+    class="flex justify-center items-center" :color="colors" width=240>
       <template #default>
-        <div class="text-xl">剩余时间</div>
-        <div class="text-12px">
+        <div class="text-4xl">剩余时间</div>
+        <div class="text-xl">
           {{ formatTime(codespaceInfo.time_quota - codespaceInfo.time_used) }}
         </div>
 
